e9d40f9cd6525077f2bdc9a54a59df34e38e91ca
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2023-07-05 14:45:14 +0300

    1

--- TravelDirections.cs
+++ TravelDirections.cs
@@ -8,5 +8,21 @@ namespace RailwayStation
 {
     internal class TravelDirections
     {
+        Random random = new Random();
+
+        private List<string> _startTravelDirections = new List<string>();
+
+        private List<string> _endTravelDirections = new List<string>();
+        public string StartTravelDirection{get;private set;}
+        public string EndTravelDirection{ get;private set;}
+
+        private void AddCitiesStartTravelDirection() 
+        {
+            _startTravelDirections.Add("Иркутск");
+            _startTravelDirections.Add("Ангарск");
+            _startTravelDirections.Add("Усолье-Сибирское");
+            _startTravelDirections.Add("Тайшет");
+            _startTravelDirections.Add("Братск");
+        }
     }
 }
