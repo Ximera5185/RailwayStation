8cde69a3eae04a3453f29e8076eaa7fff7e3ce0c
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2023-07-06 06:37:51 +0300

    1

--- RailwayStation.cs
+++ RailwayStation.cs
@@ -8,17 +8,55 @@ namespace RailwayStation
 {
     internal class RailwayStation
     {
-        private readonly TravelDirections _travelDirections = new TravelDirections();
 
+        private readonly TravelDirections _travelDirections = new TravelDirections();
+        public RailwayStation()
+        {
+            StartStantion();
+        }
         public void StartStantion()
         {
-
+            StartMenu();
         }
 
+        private void StartMenu()
+        {
+            const string _createTravelDirectionMenu = "1";
+            const string _exitProgramMenu = "5";
+
+            bool isWorckProgram = true;
+            string inputUserCommand = "";
+
+            while (isWorckProgram)
+            {
+                StatusStantion();
+
+                Console.WriteLine($"Для создания направления введите {_createTravelDirectionMenu}\n" +
+                $"Для выхода из программы введите - {_exitProgramMenu}");
+
+                inputUserCommand = Console.ReadLine();
+
+                switch (inputUserCommand)
+                {
+                    case _createTravelDirectionMenu:
+                        StartMenu();
+                        break;
+
+                    case _exitProgramMenu:
+                        isWorckProgram = false;
+                        break;
+                }
+            }
+        }
         private void ShowTravelDirection()
         {
-            Console.WriteLine($"Старт {_travelDirections.StartTravelDirection}\n" +
+            Console.WriteLine($"Создано направлене {_travelDirections.StartTravelDirection}\n" +
             $"Конкчная станция {_travelDirections.EndTravelDirection}");
+        }
+
+        private void StatusStantion()
+        {
+            ShowTravelDirection();
         }
     }
 }
